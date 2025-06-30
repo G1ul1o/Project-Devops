4eb03b5e959b61512bfd8698d5ed17c13c658eaa
MarcRG <marc.rougagnou@efrei.net>
2025-06-30 18:03:29 +0200

    Add form to predict page

--- frontend/src/pages/Predict.js
+++ frontend/src/pages/Predict.js
@@ -6,6 +6,24 @@ export default function ChampionSelect() {
     <div>
       <Header />   
       <h1>Welcome to the predict Page</h1>
+      <form className="form">
+        <label className="label">HP base:</label>
+        <input className="input" type="text" id="hp-base" name="hp-base" placeholder="Enter HP base" />
+        <label className="label">Magic damage base:</label>
+        <input className="input" type="text" id="mp-base" name="mp-base" placeholder="Enter MP base" />
+        <label className="label">Physic damage base:</label>
+        <input className="input" type="text" id="attack-base" name="attack-base" placeholder="Enter Attack base" />
+        <label className="label">Range type:</label>
+        <input className="input" type="text" id="range" name="range" placeholder="Enter Range type" />
+        <label className="label">Armor base:</label>
+        <input className="input" type="text" id="armor-base" name="armor-base" placeholder="Enter Armor base" />
+        <label className="label">Magic resist base:</label>
+        <input className="input" type="text" id="magic-resist-base" name="magic-resist-base" placeholder="Enter Magic Resist base" />
+        <label className="label">Movement speed base:</label>
+        <input className="input" type="text" id="movement-speed-base" name="movement-speed-base" placeholder="Enter Movement Speed base" />
+      </form>
+      <button type="submit" className="button">Predict</button>
+
     </div>
   );
 }
