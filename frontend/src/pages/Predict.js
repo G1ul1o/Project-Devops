c358881023824171d6da0edf78fd5814ea697f73
MarcRG <marc.rougagnou@efrei.net>
2025-06-30 23:17:44 +0200

    v3 front with udpdates on the form for testing

--- frontend/src/pages/Predict.js
+++ frontend/src/pages/Predict.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./Predict.css";
 import Header from "../component/header.js";
 
-export default function ChampionSelect() {
+export default function PredictValues() {
   const [showPopup, setShowPopup] = useState(false);
 
   const handlePredictClick = (e) => {
@@ -21,31 +21,30 @@ export default function ChampionSelect() {
       <h2 className="paraph">Enter the champion's base stats to predict their type:</h2>
       <p className="paraph">Feel free to input values and test it to see the result !</p>
 
-      <form className="form">
+      <form className="form" onSubmit={handlePredictClick}>
         <label className="label">HP base:</label>
-        <input className="input" type="text" name="hp-base" placeholder="Enter HP base" />
+        <input className="input" type="number" name="hp-base" placeholder="Enter HP base" required min="0" />
 
         <label className="label">Magic damage base:</label>
-        <input className="input" type="text" name="mp-base" placeholder="Enter MP base" />
+        <input className="input" type="number" name="mp-base" placeholder="Enter MP base" required min="0" />
 
         <label className="label">Physical damage base:</label>
-        <input className="input" type="text" name="attack-base" placeholder="Enter Attack base" />
+        <input className="input" type="number" name="attack-base" placeholder="Enter Attack base" required min="0" />
 
         <label className="label">Armor base:</label>
-        <input className="input" type="text" name="armor-base" placeholder="Enter Armor base" />
+        <input className="input" type="number" name="armor-base" placeholder="Enter Armor base" required min="0" />
 
         <label className="label">Magic resist base:</label>
-        <input className="input" type="text" name="magic-resist-base" placeholder="Enter Magic Resist base" />
+        <input className="input" type="number" name="magic-resist-base" placeholder="Enter Magic Resist base" required min="0" />
 
         <label className="label">Range type:</label>
-        <input className="input" type="text" name="range" placeholder="Enter Range type" />
+        <input className="input" type="text" name="range" placeholder="Enter Range type 'melee' or 'ranged'" pattern="melee|ranged" required />
 
         <label className="label">Movement speed base:</label>
-        <input className="input" type="text" name="movement-speed-base" placeholder="Enter Movement Speed base" />
+        <input className="input" type="number" name="movement-speed-base" placeholder="Enter Movement Speed base" required min="0" />
 
+        <button type="submit" className="button">Predict</button>
       </form>
-      <button type="submit" className="button" onClick={handlePredictClick}>Predict</button>
-
 
       {showPopup && (
         <div className="popup-overlay">
