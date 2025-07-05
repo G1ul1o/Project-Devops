db87dd7e53560c76e1ddaf3e8575e01295163ef7
G1ul1o <103744749+G1ul1o@users.noreply.github.com>
2025-07-05 14:37:16 +0200

    Server.py up and working

--- backend/server.py
+++ backend/server.py
@@ -3,12 +3,22 @@ import joblib
 import pandas as pd
 from pydantic import BaseModel
 from sklearn.preprocessing import LabelEncoder
+from fastapi.middleware.cors import CORSMiddleware
+from loguru import logger
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # ou ["*"] pour tout autoriser (déconseillé en prod)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Charger le modèle avec joblib
-model = joblib.load("XGBCl.pklassifier")
-label_encoder = joblib.load("LabelEncoderHeroType.pkl")
+model = joblib.load("../XGBClassifier.pkl")
+label_encoder = joblib.load("../LabelEncoderHeroType.pkl")
 
 range_encoder = LabelEncoder()
 range_encoder.classes_ = ['melee', 'ranged']
@@ -22,28 +32,34 @@ class ChampionFeatures(BaseModel):
     range: str  # 'melee' ou 'ranged'
     mobility: float  # on suppose qu'elle est envoyée ou par défaut 0
 
-@app.get("/predict")
+@app.post("/predict")
 def predict(features: ChampionFeatures):
+    print(features)
     try:
-        encoded_range = 0 if data.range.lower() == "melee" else 1
-
+        
+        encoded_range = 0 if features.range.lower() == "melee" else 1
+        print(encoded_range)
         input_df = pd.DataFrame([{
                 "hp_base": features.hp_base,
-                "mp_base": features.mp_base,
-                "dam_base": features.dam_base,
+                "mp_base": features.mp_base, 
                 "arm_base": features.arm_base,
                 "mr_base": features.mr_base,
+                "dam_base": features.dam_base,
                 "range": encoded_range,
                 "mobility": features.mobility
             }])
         
-        y_pred_encoded = model.predict(input_df)[0]
-        y_pred_label = label_encoder.inverse_transform([y_pred_encoded])[0]
+        logger.info(f"\ninput_def\n,{input_df}")
 
+        y_pred_encoded = model.predict(input_df)[0]
+        logger.info(f"\ny_pred_encoded\n,{y_pred_encoded}")
+        y_pred_label = label_encoder.inverse_transform([y_pred_encoded])[0]
+        print(y_pred_label)
         return {
             "prediction_encoded": int(y_pred_encoded),
             "prediction": y_pred_label
         }
     
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=400, detail=str(e))
